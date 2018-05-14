ede572affddda36aab7d94c03f1b08065fd9e1ed
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-13 21:12:28 -0500

    Purposes class

---
+++ include/Purposes.php
@@ -0,0 +1,15 @@
+<?php
+
+class Purposes {        
+    function __construct() {
+        // Connect to POS and authorization databases      
+        
+        try {
+            global $dbconfig;
+            $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
+        } catch(PDOException $e) {
+            echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
+            die();
+        }
+    }    
+}
