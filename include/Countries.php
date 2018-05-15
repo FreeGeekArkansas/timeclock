e7c41ec5a1af36b1537dac9f7a7bbef7a3b31be7
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-14 23:37:20 -0500

    Countries::showList shows some common countries first and properly selects the previously submitted country

--- include/Countries.php
+++ include/Countries.php
@@ -28,12 +28,22 @@ class Countries {
         return $results;        
     }
     
-    function showList() {
-        echo "<select name=country>\n";
+    function showList($misc='') {
+        echo "<select name=country ".$misc.">\n";
         echo '<option value="" disabled="disabled" selected="selected">Please select a Country</option>'."\n";
-        $keys = $this->list();
-        foreach ($keys as $i => $value) {   
-            echo '<option value="'.$value[0].'1">'.$value[0]."</option>\n";
+        echo '<option value="United States">United States</option>'."\n";
+        echo '<option value="Mexico">Mexico</option>'."\n";
+        echo '<option value="Canada">Canada</option>'."\n";
+        echo '<option value="" disabled="disabled">-----</option>'."\n";
+        
+        $country=getRequest('country', getSession('country'));
+        $keys = $this->list();        
+        foreach ($keys as $i => $value) {
+            if ($country === $value[0]) {
+                echo '<option value="'.$value[0].'" selected>'.$value[0]."</option>\n";
+            } else {
+                echo '<option value="'.$value[0].'">'.$value[0]."</option>\n";
+            }
         }
         echo "</select>\n";        
     }
