254d27690de426d5afd94f8593d3ba01782e1ad2
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-14 23:38:52 -0500

    added more of the application form elements, show some elements based on age, visually grouped like-elements together

--- new.php
+++ new.php
@@ -25,45 +25,96 @@
     $people = new People();
 
     if (getRequest('submit') === "apply") {        
-        $people->apply();
-        $auth->apply();
+        $person_id = $people->apply();
+        
+        $success = $auth->apply();        
+        if ($success === true) {
+            header('Location: questions.php');
+            exit();
+        }
     }
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html class="new">
 <head>
 <meta charset="utf-8">
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <title>Time clock</title>
 </head>
 <body>
-<div class="login">
+<div class="newbox">
 	<h1>New application</h1>
     <form method="post" enctype="application/x-www-form-urlencoded" action="new.php">
-  <?php
-  $people->showInput('first_name', 'First Name', 'text', true);
-  $keys = array('middle_name','last_name','address1','address2','city','state','state_other','zipcode','country','phone','email');
-  $placeholders = array('Middle Name','Last Name','Address Line #1','Address Line #2','City','State','State','Zipcode','Country','Phone','Email');
-  foreach ($keys as $i => $value) {
-      if ($value === 'country') {
-          $countries = new Countries();
-          $countries->showList();
-      } else {
-        $people->showInput($value, $placeholders[$i]);
-      }
-  }
-  $people->showInput('dob', 'DOB', 'date', true);
-            
-//            ,'guardian_first_name','guardian_middle_name', 'guardian_last_name','guardian_phone','guardian_relationship',
-//            'emergency_first_name','emergency_middle_name', 'emergency_last_name','emergency_phone','emergency_relationship',
-//            'type');     	
+<?php
+
+    $people->showInput('first_name', 'First Name', 'text', true);
+    $people->showInput('middle_name', 'Middle Name', 'text', false);
+    $people->showInput('last_name', 'Last Name', 'text', false);
+    echo '<div class="label">Date of Birth: ';
+    $people->showInput('dob', 'DOB', 'date', true);
+    echo "</div>\n";
+    
+    $people->showInput('email', 'Email address', 'text', false);
+    $people->showInput('phone', 'Phone number', 'text', false);
+    $people->showInput('address1', 'Address Line #1');
+    $people->showInput('address2', 'Address Line #2');
+    $people->showInput('city', 'City');
+    $people->showInput('zipcode', 'Zipcode');
+       
+    $states = new States();
+    $states->showList('class="label"');
+    
+    $countries = new Countries();
+    $countries->showList('class="input"');
 ?>
-     	<input type="text" name="username" placeholder="Username" value="<?php echo $auth->get('username'); ?>" /><?php showError($auth->error('username')); ?>     	
-     	<input type="password" name="pin" placeholder="PIN" size=4 value="<?php echo $auth->get('pin'); ?>" required="required" /><?php showError($auth->error('pin')); ?>
+<h3 class="label">Emergency Contact's Name and Phone number</h3>
+<?php
+    $people->showInput('emergency_first_name', 'First Name', 'text', true);
+    $people->showInput('emergency_middle_name', 'Middle Name', 'text', false);
+    $people->showInput('emergency_last_name', 'Last Name', 'text', false);
+    $people->showInput('emergency_phone', 'Phone number', 'text', false);
+    $people->showInput('emergency_relationship', 'Relationship', 'text', false);
+    
+    //            ,'guardian_first_name','guardian_middle_name', 'guardian_last_name','guardian_phone','guardian_relationship',
+    //            'emergency_first_name','emergency_middle_name', 'emergency_last_name','emergency_phone','emergency_relationship',
+    //            'type');
+    
+    $dob = $people->get('dob');
+    if ($dob !== null) {
+        $systemTimeZone = exec('date +%Z');
+        $sysdtz = new DateTimeZone($systemTimeZone);
+        
+        $dt = new DateTime($dob,$sysdtz);        
+        $now = new DateTime(null,$sysdtz);
+        
+        $di = date_diff($dt, $now);
+        $age = $di->y;
+        if ($age < 18) {
+?>
+            <h3 class="label">Guardian's Name and Phone number</h3>
+<?php 
+            $people->showInput('guardian_first_name', 'First Name', 'text', false);
+            $people->showInput('guardian_middle_name', 'Middle Name', 'text', false);
+            $people->showInput('guardian_last_name', 'Last Name', 'text', false);
+            $people->showInput('guardian_phone', 'Phone number', 'text', false);
+            $people->showInput('guardian_relationship', 'Relationship', 'text', false);
+?>
+<div class="label">Since you are not yet 18 we need to know who your parent or guardian is.</div>
+
+<?php
+        }
+    }
+?>
+
+		<div><h3 class="label">Your username, PIN and Password</h3>
+     	<input type="text" name="username" placeholder="Username" value="<?php echo $auth->get('username'); ?>" required="required" /><?php showError($auth->error('username')); ?>
+     	<input type="password" name="pin" placeholder="PIN" size=4 maxlength=4 value="<?php echo $auth->get('pin'); ?>" required="required" /><?php showError($auth->error('pin')); ?><div class="label">The pin is a 4 digit number that can be used to quickly clock-in or out.</div>
+     	</div>
      	<p>
      	<input type="password" name="password" placeholder="Password" value="<?php echo $auth->get('password'); ?>" required="required" /><?php showError($auth->error('password')); ?>
-        <input type="password" name="password2" placeholder="Repeat password" value="<?php echo $auth->get('password2'); ?>" required="required" /><?php showError($auth->error('password2')); ?></p>
+        <input type="password" name="password2" placeholder="Repeat password" value="<?php echo $auth->get('password2'); ?>" required="required" /><?php showError($auth->error('password2')); ?>
+        <div class="label">Your password is used to authenticate you when you want to change your information.</div></p>
         <button type="submit" name="submit" value="apply" class="btn btn-primary btn-block btn-large">Apply</button>        
     </form>
 </div>
