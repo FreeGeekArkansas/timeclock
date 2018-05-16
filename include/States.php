85a87ad8aacc420ee3dfcbb075120d701bfff63c
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-15 22:33:45 -0500

    States now allows quick pick countries to stay selected

--- include/States.php
+++ include/States.php
@@ -29,25 +29,34 @@ class States {
     }
     
     function showList($misc = '') {
+        $selected=false;
+        $state=getRequest('state', getSession('state'));
         echo "<select name=state ".$misc.">\n";
         echo '<option value="" disabled="disabled" selected="selected">Please select a State</option>'."\n";
-        echo '<option value="Arkansas">Arkansas</option>'."\n";
-        echo '<option value="Oklohoma">Oklohoma</option>'."\n";
-        echo '<option value="Kansas">Kansas</option>'."\n";        
-        echo '<option value="" disabled="disabled">-----</option>'."\n";
         
-        $state=getRequest('state', getSession('state'));
+        $keys = Array('Arkansas', 'Oklahoma', 'Missouri');
+        foreach ($keys as $i => $value) {
+            if ($state === $value) {
+                echo '<option value="'.$value.'" selected>'.$value."</option>\n";
+                $selected=true;
+            } else {
+                echo '<option value="'.$value.'">'.$value."</option>\n";
+            }
+        }
+        
+        echo '<option value="" disabled="disabled">-----</option>'."\n";
+                
         if ($state === 'Other') {
-            echo '<option value="Other" onselect="showother();" selected>A state or province not listed here</option>'."\n";            
+            echo '<option value="Other" onselect="showStateOther();" selected>A state or province not listed here (Other)</option>'."\n";            
         } else {
-            echo '<option value="Other" onselect="showother();">A state or province not listed here</option>'."\n";
+            echo '<option value="Other" onselect="showStateOther();">A state or province not listed here (Other)</option>'."\n";
         }
         echo '<option value="" disabled="disabled">-----</option>'."\n";
         
         
         $keys = $this->list();
         foreach ($keys as $i => $value) {
-            if ($state === $value[0]) {
+            if ($state === $value[0] && $selected === false) {
                 echo '<option value="'.$value[0].'" selected>'.$value[0]."</option>\n";
             } else {
                 echo '<option value="'.$value[0].'">'.$value[0]."</option>\n";
