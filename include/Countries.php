dfb6995304ad5ac9ab7cd546a60ed51aa31d8313
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-15 22:33:31 -0500

    Countries now allows quick pick countries to stay selected

--- include/Countries.php
+++ include/Countries.php
@@ -29,17 +29,33 @@ class Countries {
     }
     
     function showList($misc='') {
+        $selected=false;
+        $country=getRequest('country', getSession('country'));
         echo "<select name=country ".$misc.">\n";
         echo '<option value="" disabled="disabled" selected="selected">Please select a Country</option>'."\n";
-        echo '<option value="United States">United States</option>'."\n";
-        echo '<option value="Mexico">Mexico</option>'."\n";
-        echo '<option value="Canada">Canada</option>'."\n";
+        
+        $keys = Array('United States', 'Mexico', 'Canada');
+        foreach ($keys as $i => $value) {
+            if ($country === $value) {
+                echo '<option value="'.$value.'" selected>'.$value."</option>\n";
+                $selected=true;
+            } else {
+                echo '<option value="'.$value.'">'.$value."</option>\n";
+            }
+        }
+        
         echo '<option value="" disabled="disabled">-----</option>'."\n";
         
-        $country=getRequest('country', getSession('country'));
+        if ($country === 'Other') {
+            echo '<option value="Other" onselect="showCountryOther();" selected>A country not listed here (Other)</option>'."\n";
+        } else {
+            echo '<option value="Other" onselect="showCountryOther();">A country not listed here (Other)</option>'."\n";
+        }
+        echo '<option value="" disabled="disabled">-----</option>'."\n";
+        
         $keys = $this->list();        
         foreach ($keys as $i => $value) {
-            if ($country === $value[0]) {
+            if ($country === $value[0] && $selected === false) {
                 echo '<option value="'.$value[0].'" selected>'.$value[0]."</option>\n";
             } else {
                 echo '<option value="'.$value[0].'">'.$value[0]."</option>\n";
