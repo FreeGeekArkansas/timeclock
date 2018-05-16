61ce9f83e97b4e989b005c910ada027c567441f6
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-16 00:13:52 -0500

    added copyright and license info.

--- include/default.inc.php
+++ include/default.inc.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ Copyright (C) 2018  Jared H. Hudson
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 header("Cache-Control: max-age=0,no-cache,no-store,post-check=0,pre-check=0");
 
@@ -26,11 +42,11 @@ spl_autoload_register(function ($class_name) {
     
 function showError($msg) {
     if ($msg) {
-        echo '<div class=error>'.$msg.'</div>';
+        echo '<span class=error>'.$msg.'</span> ';
     }
 }
 
-function getRequest($key, $defaultValue = null) {
+function getRequest($key, $defaultValue = '') {
     if (isset($_REQUEST[$key])) {
         return $_REQUEST[$key];
     } else {
@@ -38,7 +54,7 @@ function getRequest($key, $defaultValue = null) {
     }
 }
 
-function getSession($key, $defaultValue = null) {
+function getSession($key, $defaultValue = '') {
     if (isset($_SESSION[$key])) {
         return $_SESSION[$key];
     } else {
