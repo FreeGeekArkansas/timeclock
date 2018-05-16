35da15098d8cea638f286daadf9b8216a99e5b99
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-16 00:15:44 -0500

    added license and support for using DB class connection

--- include/People.php
+++ include/People.php
@@ -1,17 +1,38 @@
 <?php
+/*
+ Copyright (C) 2018  Jared H. Hudson
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 class People {        
-    function __construct() {
-        try {
-            global $dbconfig;
-            $this->db = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
-        } catch(PDOException $e) {
-            echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
-            die();
+    function __construct(DB $db = null) {
+        if ($db === null || $db->authdb->getAttribute(PDO::ATTR_CONNECTION_STATUS) !== 'Connection OK; waiting to send.') {
+            try {
+                global $dbconfig;
+                $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
+                $this->authdb->beginTransaction();
+            } catch(PDOException $e) {
+                echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
+                die();
+            }
+        } else {
+            $this->authdb = $db->authdb;
         }
         
-        $this->variables = array();        
-        
+        $this->variables = array();
     }
     
     function list() {
@@ -46,6 +67,37 @@ class People {
             $this->copy('emergency_phone', 'guardian_phone');
             $this->copy('emergency_relationship', 'guardian_relationship');
         }
+        
+        $form_completed_status = true;
+        $required_keys = array('first_name','dob','email','emergency_first_name','emergency_phone');
+        foreach ($required_keys as $i => $value) {
+            if (empty($this->variables[$value])) {
+                $this->var_errors[$value] = 'required';
+                $form_completed_status = false;
+            }
+        }
+        
+        if ($form_completed_status === true) {
+            $stmt = $this->authdb->prepare('INSERT INTO people (first_name,middle_name,last_name,address1,address2,city,state,state_other,zipcode,country,country_other,phone,email,dob,guardian_first_name,guardian_middle_name, guardian_last_name,guardian_phone,guardian_relationship,emergency_first_name,emergency_middle_name, emergency_last_name,emergency_phone,emergency_relationship) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+
+            $vars = array();
+            foreach ($keys as $i => $value) {
+                if (isset($this->variables[$value])) {
+                    array_push($vars, $this->variables[$value]);
+                } else {
+                    array_push($vars, null);
+                }
+            }
+            
+            $success = $stmt->execute($vars);
+            if ($success === true) {
+                $_SESSION['person_id'] = lastInsertId();
+            } else {
+                return false;
+            }
+        }
+        
+        return $form_completed_status;
     }
     
     function get($var) {
@@ -62,6 +114,9 @@ class People {
     }
     
     function error($var) {
+        if (isset($this->var_errors[$var])) {
+            return $this->var_errors[$var];
+        }
         return '';
     }
     
@@ -73,6 +128,5 @@ class People {
         }
         echo '/>';
         showError($this->error($name));
-        echo "\n";
     }
 }
