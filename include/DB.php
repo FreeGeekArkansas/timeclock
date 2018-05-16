268e9e89d427c50c924f97c9d5a2bd18bcd404c3
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-16 00:14:37 -0500

    added DB class to provide common point DB session and begin/commit/rollback

---
+++ include/DB.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ Copyright (C) 2018  Jared H. Hudson
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class DB {
+    function __construct() {        
+        try {
+            global $dbconfig;
+            $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
+            $this->authdb->beginTransaction();
+        } catch(PDOException $e) {
+            echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
+            die();
+        }
+    }
+    
+    function commit() {
+        return $this->authdb->commit();
+    }
+}
+?>
