06f790603687642aaf19695f0ea78bc071958c51
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-22 23:48:13 -0500

    pass PDO by reference, use state and country lookup functions, added error checking and pre-insert of emergency/guardian contact before main person row

--- include/People.php
+++ include/People.php
@@ -17,20 +17,8 @@
  */
 
 class People {
-    function __construct(DB $db = null) {
-        if ($db === null || $db->authdb->getAttribute(PDO::ATTR_CONNECTION_STATUS) !== 'Connection OK; waiting to send.') {
-            try {
-                global $dbconfig;
-                $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
-                $this->authdb->beginTransaction();
-            } catch(PDOException $e) {
-                echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
-                die();
-            }
-        } else {
-            $this->authdb = $db->authdb;
-        }
-        
+    function __construct(PDO &$authdb) {
+        $this->authdb =& $authdb;
         $this->variables = array();
     }
     
@@ -94,9 +82,52 @@ class People {
         }
         
         if ($form_completed_status === true) {
-            $stmt = $this->authdb->prepare('INSERT INTO people (first_name,middle_name,last_name,address1,address2,city,state,state_other,zipcode,country,country_other,phone,email,dob,guardian_first_name,guardian_middle_name, guardian_last_name,guardian_phone,guardian_relationship,emergency_first_name,emergency_middle_name, emergency_last_name,emergency_phone,emergency_relationship) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+            
+            // lookup state
+            $states = new States($this->authdb);
+            $state_id = $states->lookup($state = $this->variables['state']);
+            if ($state_id === 0) {
+                $this->var_errors['state'] = 'State \''.$this->variables['state'].'\' unrecognized.';
+                return false;
+            } else {
+                $this->variables['state_id'] = $state_id;
+            }
 
+            // lookup country
+            $countries = new Countries($this->authdb);
+            $country_id = $countries->lookup($country = $this->variables['country']);
+            if ($country_id === 0) {
+                $this->var_errors['country'] = 'Country \''.$this->variables['country'].'\' unrecognized.';
+                return false;
+            } else {
+                $this->variables['country_id'] = $country_id;
+            }
+
+            if (isset($this->variables['guardian_first_name']) && !empty($this->variables['guardian_first_name'])) {
+                $stmt = $this->authdb->prepare('INSERT INTO people (first_name,middle_name,last_name,phone,person_type) VALUES (?,?,?,?,?)');
+                $keys = array('guardian_first_name','guardian_middle_name','guardian_last_name','guardian_phone','guardian_type');
+                $vars = array();
+                $this->variables['guardian_type'] = 'guardian contact';
+                foreach ($keys as $i => $value) {
+                    if (isset($this->variables[$value])) {
+                        array_push($vars, $this->variables[$value]);
+                    } else {
+                        array_push($vars, null);
+                    }
+                }            
+                $success = $stmt->execute($vars);
+                if ($success === true) {
+                    $this->variables['guardian_id'] = $this->authdb->lastInsertId('people_person_id_seq');
+                } else {
+                    $error = $stmt->errorInfo();
+                    $this->var_errors['guardian'] = $error[0].' '.$error[1].' '.$error[2];
+                }
+            }
+            
+            $stmt = $this->authdb->prepare('INSERT INTO people (first_name,middle_name,last_name,phone,person_type) VALUES (?,?,?,?,?)');
+            $keys = array('emergency_first_name','emergency_middle_name', 'emergency_last_name','emergency_phone','emergency_type');
             $vars = array();
+            $this->variables['emergency_type'] = 'emergency contact';
             foreach ($keys as $i => $value) {
                 if (isset($this->variables[$value])) {
                     array_push($vars, $this->variables[$value]);
@@ -104,13 +135,37 @@ class People {
                     array_push($vars, null);
                 }
             }
-            
             $success = $stmt->execute($vars);
             if ($success === true) {
-                $_SESSION['person_id'] = lastInsertId();
+                $this->variables['emergency_id'] = $this->authdb->lastInsertId('people_person_id_seq');
+            } else {
+                echo 'fail';
+                $error = $stmt->errorInfo();
+                $this->var_errors['emergency'] = $error[0].' '.$error[1].' '.$error[2];                
+            }
+            $stmt->closeCursor();
+            
+            $stmt = $this->authdb->prepare('INSERT INTO people (first_name,middle_name,last_name,address1,address2,city,state_id,state_other,zipcode,country_id,country_other,phone,email,dob,guardian_id,guardian_relationship,emergency_id,emergency_relationship,person_type) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+            
+            $keys = array('first_name','middle_name','last_name','address1','address2','city','state_id','state_other','zipcode','country_id',
+                'country_other','phone','email','dob','guardian_id','guardian_relationship','emergency_id','emergency_relationship','type');
+            $vars = array();
+            $this->variables['type'] = 'volunteer';
+            foreach ($keys as $i => $value) {
+                if (isset($this->variables[$value]) && !empty($this->variables[$value])) {
+                    $stmt->bindValue($i+1, $this->variables[$value]);
+                } else {
+                    $stmt->bindValue($i+1, null, PDO::PARAM_NULL);
+                }
+            }
+            $success = $stmt->execute();
+            
+            if ($success === true) {
+                $_SESSION['person_id'] = $this->authdb->lastInsertId('people_person_id_seq');                
                 return true;
             } else {
-                // Failed to insert records into DB
+                $error = $stmt->errorInfo();
+                $this->var_errors['people'] = $error[0].' '.$error[1].' '.$error[2];
                 return false;
             }
         }
