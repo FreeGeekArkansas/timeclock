7451cb96de0f584f421e57d993f994cac87057bd
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-23 01:01:25 -0500

    beginning to questions module

---
+++ questions.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ Copyright (C) 2018  Jared H. Hudson
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+{
+    include_once 'include/default.inc.php';
+    session_start();
+    
+    // If user is not already logged in then take them to login page
+    if (getSession('authorized') != 1) {         
+        header('Location: login.php');
+        exit();
+    }
+    
+    
+}
+?>
+<!DOCTYPE html>
+<html class="new">
+<head>
+<meta charset="utf-8">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<title>Questions</title>
+</head>
+<script></script>
+<body>
+</body>
+<script></script>
+</html>
