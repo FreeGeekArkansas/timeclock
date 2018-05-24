374a9fe9d9a7637df25368ee6482db9f3cedb6a8
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-23 23:51:58 -0500

    adds some default FGAR questions

--- sqitch/deploy/questions.sql
+++ sqitch/deploy/questions.sql
@@ -19,5 +19,80 @@ CREATE
             modified_on timestamptz,
             modified_by INT REFERENCES people(person_id)
         );
+INSERT
+    INTO
+        questions(
+            question,
+            optional,
+            answer_type,
+            created_on
+        )
+    VALUES(
+        'Do you have any work related restrictions you''d like us to know about?',
+        TRUE,
+        'text',
+        now()- '1 week'::INTERVAL
+    );
 
-COMMIT;
+INSERT
+    INTO
+        questions(
+            question,
+            optional,
+            answer_type,
+            created_on
+        )
+    VALUES(
+        'Are you legally allowed to be around children?',
+        FALSE,
+        'boolean',
+        now()- '1 week'::INTERVAL
+    );
+
+INSERT
+    INTO
+        questions(
+            question,
+            optional,
+            answer_type,
+            created_on
+        )
+    VALUES(
+        'Do you have any restraining orders against you?',
+        FALSE,
+        'boolean',
+        now()- '1 week'::INTERVAL
+    );
+   
+   
+   INSERT
+    INTO
+        questions(
+            question,
+            optional,
+            answer_type,
+            created_on
+        )
+    VALUES(
+        'Are you here to complete community service hours?',
+        TRUE,
+        'boolean',
+        now()- '1 week'::INTERVAL
+    );
+
+   INSERT
+    INTO
+        questions(
+            question,
+            optional,
+            answer_type,
+            created_on
+        )
+    VALUES(
+        'What skills, training or knowledge (computer related or not) would you be interested in sharing with Free Geek Arkansas?',
+        TRUE,
+        'text',
+        now()- '1 week'::INTERVAL
+    );
+
+   COMMIT;
