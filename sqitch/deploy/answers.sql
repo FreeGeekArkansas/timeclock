c310b0aab2c87b2adf4f7502205f064eb1e19299
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-23 23:49:55 -0500

    formatted

--- sqitch/deploy/answers.sql
+++ sqitch/deploy/answers.sql
@@ -5,12 +5,11 @@ CREATE
     TABLE
         answers(
             answer_id serial PRIMARY KEY,
-	    question_id int references questions,
+            question_id INT REFERENCES questions,
             text_answer text,
             bool_answer bool,
-            answered_by int REFERENCES people(person_id),
+            answered_by INT REFERENCES people(person_id),
             answered_on timestamptz
-
         );
 
 COMMIT;
