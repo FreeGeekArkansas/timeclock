ba31fdc4d104a6fffe70b104b2a2c22e33f56034
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-23 23:49:43 -0500

    Answers extends Form now

--- include/Answers.php
+++ include/Answers.php
@@ -16,16 +16,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Answers {        
-    function __construct() {
-        // Connect to POS and authorization databases      
-        
-        try {
-            global $dbconfig;
-            $this->authdb = new PDO($dbconfig['auth']['dsn'], $dbconfig['auth']['username'], $dbconfig['auth']['password'], $dbconfig['auth']['options']);
-        } catch(PDOException $e) {
-            echo 'Error connecting to auth DB. Caught exception: [',  $e->getMessage(), "]\n";
-            die();
-        }
-    }   
+class Answers extends Form {        
+    function __construct(PDO &$authdb) {
+        $this->authdb = &$authdb;
+    }
 }
