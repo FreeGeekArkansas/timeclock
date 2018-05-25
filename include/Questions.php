5bc18fabcc15a4e36afd0dd83deaba19a7a87491
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-05-25 01:41:48 -0500

    Question class gets Questions/Answers for person_id in constructor, newQuestions() now performs SQL count, apply method identifies when required questions have not been answered

--- include/Questions.php
+++ include/Questions.php
@@ -4,14 +4,34 @@ class Questions extends Form {
     function __construct(PDO &$authdb, &$person_id) {
         $this->authdb = &$authdb;
         $this->person_id = &$person_id;
+        $this->newQuestionCount = 0;
+        
+        $stmt = $this->authdb->prepare('SELECT * FROM questions q LEFT JOIN answers a ON (a.answered_by=:person_id AND q.question_id=a.question_id);');
+        $stmt->bindParam(':person_id', $this->person_id, PDO::PARAM_INT);
+        $success = $stmt->execute();
+        if ($success === true) {
+            $this->questions = $stmt->fetchAll();            
+        } else {
+            print_r($stmt->errorInfo());
+        }
+        $stmt->closeCursor();
     }
     
     function newQuestions() {
-        $stmt = $this->authdb->prepare('SELECT * FROM questions q LEFT JOIN answers a ON (a.answered_by=:person_id AND q.question_id=a.question_id AND a.answer_id is null);');
-        $stmt->bindParam(':person_id', $this->person_id);
-        $success = $stmt->execute();
-        if ($success === true) {
-            $this->questions = $stmt->fetchAll();
+        if ($this->newQuestionCount == 0) {
+            $stmt = $this->authdb->prepare('SELECT count(*) FROM questions q LEFT JOIN answers a ON (a.answered_by=:person_id AND q.question_id=a.question_id) where answer_id is null;');
+            $stmt->bindParam(':person_id', $this->person_id);
+            $success = $stmt->execute();
+            if ($success === true) {
+                $results = $stmt->fetchObject();
+                $this->newQuestionCount = $results->count;
+            } else {
+                print_r($stmt->errorInfo());
+            }
+            $stmt->closeCursor();
+        }
+        
+        if ($this->newQuestionCount > 0) {
             return true;
         } else {
             return false;
@@ -23,20 +43,19 @@ class Questions extends Form {
         $l = count($this->questions);
         for ($i=0; $i < $l; ++$i) {
             array_push($keys, $this->questions[$i][0]);
-        }        
+        }
         
         $form_completed_status = true;
         foreach ($keys as $i => $value) {
             $this->variables[$value] = getRequest($value);
-            if (empty($this->variables[$value]) && $this->questions[$i]['optional'] = 'f') {
-                $error = &$this->error($value);
-                $error = 'required';
+
+            if (empty($this->variables[$value]) && $this->questions[$i]['optional'] == null) {
+                $error = $this->setError($value, 'required');
                 $form_completed_status = false;
             }
         }
         
         if ($form_completed_status === true) {
-            echo 'found';
             $stmt = $this->authdb->prepare('INSERT INTO authentication (person_id,username,password,pin) VALUES (?,?,?,?)');
             $hashed_password = password_hash($this->variables['password'], PASSWORD_DEFAULT, array('cost' => 12));
             $hashed_pin = password_hash($this->variables['pin'], PASSWORD_DEFAULT, array('cost' => 12));
