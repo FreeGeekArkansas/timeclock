00b4edf01aeaaaa057b82bd4c1de9ab281b1fd9c
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2019-03-10 22:03:34 -0500

    Timeclock->status($limit = 'ALL', $offset = 0) uses LIMIT and OFFSET status fetches all rows up to LIMIT now Timeclock->clockin() now clocks you in

--- include/Timeclock.php
+++ include/Timeclock.php
@@ -16,41 +16,75 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class Timeclock extends Form {        
+class Timeclock extends Form {
     function __construct(PDO &$authdb, &$person_id) {
         $this->authdb = &$authdb;
         $this->person_id = &$person_id;
+        $this->timeclock = Array();
     }
-    
-    function status() {
-        $stmt = $this->authdb->prepare('SELECT DISTINCT * FROM timeclock where person_id=:person_id;');
+
+    function status($limit = 'ALL', $offset = 0) {
+    	if (is_numeric($limit)) {
+        	$stmt = $this->authdb->prepare("SELECT date_trunc('seconds',t.clock_in)::timestamp as clock_in,date_trunc('seconds',t.clock_out)::timestamp as clock_out,p.purpose,date_trunc('seconds', age(t.clock_out,t.clock_in)) as length FROM timeclock as t,purposes as p WHERE person_id=:person_id and t.purpose_id = p.purpose_id ORDER BY person_id, clock_in DESC limit :limit OFFSET :offset;");
+        	$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+    	} else {
+			$stmt = $this->authdb->prepare('SELECT t.*,p.purpose,date_trunc(\'seconds\', age(t.clock_out,t.clock_in)) as length FROM timeclock as t,purposes as p WHERE person_id=:person_id and t.purpose_id = p.purpose_id ORDER BY person_id, clock_in DESC OFFSET :offset;');
+    	}
         $stmt->bindParam(':person_id', $this->person_id, PDO::PARAM_INT);
+        $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
         $success = $stmt->execute();
         if ($success === true) {
-            $this->timeclock = $stmt->fetchAll();
+        	if ($stmt->rowCount()) {
+        		$this->timeclock = $stmt->fetchAll();
+        		if (!empty($this->timeclock[0]['clock_in']) && empty($this->timeclock[0]['clock_out'])) {
+        			return true;
+        		}
+        	}
         } else {
             print_r($stmt->errorInfo());
         }
         $stmt->closeCursor();
-        
-        print_r($this->timeclock);
+        return false;
     }
-    
+
     function clockin() {
-        $this->authdb->prepare('INSERT INTO timeclock (clock_in, purpose_id, person_id) VALUES (\'now\',?,?);');
-    }
-    
-    function clockout() {
+    	$purpose_id = getRequest('purpose_id', 1);
+        $stmt = $this->authdb->prepare('INSERT INTO timeclock (clock_in, purpose_id, person_id) VALUES (\'now\',?,?);');
+        $success = $stmt->execute(array($purpose_id, $this->person_id));
+        if ($success !== true) {
+        	print('<b>ERROR: Error in clockin.</b><br>');
+        	print_r($stmt->errorInfo());
+        	return false;
+        }
         
+        return true;
     }
-    
+
+    function clockout() {
+    	$clocked_in = $this->status();
+    	if ($clocked_in) {
+    		$timeclock_id = $this->timeclock[0]['timeclock_id'];
+    		
+    		$stmt = $this->authdb->prepare('UPDATE timeclock SET (clock_out) = (\'now\') WHERE timeclock_id = ?;');
+    		$success = $stmt->execute(array($timeclock_id));
+    		
+    		if ($success !== true) {
+    			print('<b>ERROR: Error in clockout.</b><br>');
+    			print_r($stmt->errorInfo());
+    			return false;
+    		}
+    		return true;
+    	}
+    	return false;
+    }
+
     function apply() {
         $keys = array();
         $l = count($this->questions);
         for ($i=0; $i < $l; ++$i) {
             array_push($keys, $this->questions[$i][0]);
         }
-        
+
         $form_completed_status = true;
         foreach ($keys as $i => $value) {
             $this->variables[$value] = getRequest($value);
@@ -60,11 +94,11 @@ class Timeclock extends Form {
                 $form_completed_status = false;
             }
         }
-        
+
         if ($form_completed_status === true) {
             $bool_stmt = $this->authdb->prepare('INSERT INTO answers (question_id, bool_answer, answered_by) VALUES (?,?,?);');
             $text_stmt = $this->authdb->prepare('INSERT INTO answers (question_id, text_answer, answered_by) VALUES (?,?,?);');
-            
+
             foreach ($keys as $i => $value) {
                 if ($this->questions[$i]["answer_type"] === "boolean") {
                     $stmt = $bool_stmt;
@@ -76,7 +110,7 @@ class Timeclock extends Form {
                     print('$value = '.$value.'<br>');
                     print_r($this->questions);
                     print_r($this->variables);
-                    return false;                        
+                    return false;
                 }
                 $success = $stmt->execute(array($value, $this->variables[$value], $this->person_id));
                 if ($success !== true) {
@@ -88,5 +122,5 @@ class Timeclock extends Form {
         }
         return true;
     }
-    
+
 }
